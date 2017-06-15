abe3fb7e6d9212a686f41b8edc8b8ff313b202d1
Christian Horsdal <c.horsdal@gmail.com>
2017-06-15 22:35:20 +0200

    update ch04 to netcore 1.1 and nancy 2.0.0-clinteastwood

--- src/Chapter04/LoyaltyProgram/Startup.cs
+++ src/Chapter04/LoyaltyProgram/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace LoyaltyProgram
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+    using Nancy.Owin;
+    
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -20,10 +15,7 @@ namespace LoyaltyProgram
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseOwin().UseNancy();
         }
     }
 }
